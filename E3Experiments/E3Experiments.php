da187664452e8af6a3360edbc9251ff3c2019455
Ori Livneh <ori.livneh@gmail.com>
2012-05-31 01:37:38 +0100

    Add nested extension for E3 timestamp experiment
    Including this extension here is a kludge, meant to route around a delay
    in Gerrit repo creation that is currently blocking the E3 team. The
    LastModified extension itself has not been modified and its
    functionality has not been impacted.
    
    This experiment is shown to 50% of editors, on a sample factor of 0.006 of
    articles. Those in the experiment will see the time the article was last
    modified presented in a human-readable format in the article's header.
    Clicking on the timestamp will log the event and redirect the user to
    the article's history page.
    
    Specific information regarding this experiment can be found on its Meta
    page: https://meta.wikimedia.org/wiki/Research:Timestamp_position_modification
    
    Change-Id: Ic57fc3cacbf68d21cb493260c8b566608c681c30

---
+++ E3Experiments/E3Experiments.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Wikimedia Foundation
+ *
+ * LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * @author    Ori Livneh <ori@wikimedia.org>
+ */
+
+$wgExtensionCredits['specialpage'][] = array(
+  'path' => __FILE__,
+  'name' => 'E3 Experiment Loader',
+  'version' => '0.1',
+  'url' => 'https://www.mediawiki.org/wiki/Extension:E3Experiments',
+  'author' => array( 'Ori Livneh' ),
+  'descriptionmsg' => 'E3 Experiment Loader',
+);
+
+$wgResourceModules['ext.E3Experiments'] = array(
+  'scripts' => array( 'js/murmurhash2_gc.js', 'js/ext.E3Experiments.Timestamp.js' ),
+  'dependencies' => array( 'jquery.clickTracking' ),
+  'localBasePath' => dirname( __FILE__ ),
+  'remoteExtPath' => 'E3Experiments'
+);
+
+$wgHooks['BeforePageDisplay'][] = 'loadExperiments';
+
+function loadExperiments( $out ) {
+  $out->addModules( 'ext.E3Experiments' );
+
+  // Hack. If we defer loading to the Resource Loader, we get FOUCs.
+  $out->addInlineStyle( '.lastmodified { display: none; }' );
+  return true;
+}
