7a496bc752819804d02838ade0c0bdbc0ab0e1c1
raymond <raimond.spekking@gmail.com>
2012-06-01 22:10:00 +0200

    Add i18n file
    Fix extension credit
    Spaces2Tabs
    
    Change-Id: Id3e0c2a863734f1d945259d365e5e459d2612d3a

---
+++ E3Experiments/E3Experiments.i18n.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Wikimedia Foundation
+ *
+ * LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * @author    Ori Livneh <ori@wikimedia.org>
+ */
+
+/**
+ * Internationalisation for E3Experiments extension
+ */
+
+$messages = array();
+
+/** English
+ * @author Ori Livneh
+ */
+$messages['en'] = array(
+	'e3experiments-desc' => 'Provides a scaffold for members of the [https://www.mediawiki.org/wiki/Editor_Engagement_Experiments Editor Engagement Experiments] to use when deploying simple experiments',
+);
+
+/** Message documentation (Message documentation)
+ * @author Ori Livneh
+ */
+$messages['qqq'] = array(
+	'e3experiments-desc' => '{{desc}}',
+);
+
--- E3Experiments/E3Experiments.php
+++ E3Experiments/E3Experiments.php
@@ -18,28 +18,28 @@
  */
 
 $wgExtensionCredits['specialpage'][] = array(
-  'path' => __FILE__,
-  'name' => 'E3 Experiment Loader',
-  'version' => '0.1',
-  'url' => 'https://www.mediawiki.org/wiki/Extension:E3Experiments',
-  'author' => array( 'Ori Livneh' ),
-  'descriptionmsg' => 'E3 Experiment Loader',
+	'path' => __FILE__,
+	'name' => 'E3 Experiment Loader',
+	'version' => '0.1',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:E3Experiments',
+	'author' => array( 'Ori Livneh' ),
+	'descriptionmsg' => 'e3experiments-desc',
 );
 
 // This extension requires that the ClickTracking extension is installed
 $wgResourceModules['ext.E3Experiments'] = array(
-  'scripts' => array( 'js/murmurhash2_gc.js', 'js/ext.E3Experiments.Timestamp.js' ),
-  'dependencies' => array( 'jquery.clickTracking' ),
-  'localBasePath' => dirname( __FILE__ ),
-  'remoteExtPath' => 'LastModified/E3Experiments'
+	'scripts' => array( 'js/murmurhash2_gc.js', 'js/ext.E3Experiments.Timestamp.js' ),
+	'dependencies' => array( 'jquery.clickTracking' ),
+	'localBasePath' => dirname( __FILE__ ),
+	'remoteExtPath' => 'LastModified/E3Experiments'
 );
 
 $wgHooks['BeforePageDisplay'][] = 'loadExperiments';
 
 function loadExperiments( $out ) {
-  $out->addModules( 'ext.E3Experiments' );
+	$out->addModules( 'ext.E3Experiments' );
 
-  // Hack. If we defer loading to the Resource Loader, we get FOUCs.
-  $out->addInlineStyle( '#mwe-lastmodified { display: none; }' );
-  return true;
+	// Hack. If we defer loading to the Resource Loader, we get FOUCs.
+	$out->addInlineStyle( '#mwe-lastmodified { display: none; }' );
+	return true;
 }
